1173f1d0dfe87907259401e99e7534309a4454f7
『ϻᴿ』☯Sʜᴀᴅօw <92666116+FASTmovie@users.noreply.github.com>
2022-12-03 12:11:00 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('🤥 Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('🔔 Updates', url='https://t.me/LazyDeveloper')
+            InlineKeyboardButton('😍update channel😍', url='https://t.me/bhiman1234'),
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
